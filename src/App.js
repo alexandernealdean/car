38cd8521416d362a9483a7fe12534aab6e8958e1
alexandernealdean <54088704+alexandernealdean@users.noreply.github.com>
2022-05-16 01:20:34 -0400

    added car and camera movement, added 3d portfolio text

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Physics, useCylinder, usePlane } from '@react-three/cannon'
 import { OrbitControls, Environment } from '@react-three/drei'
-import Vehicle from './Vehicle'
+import Vehicle from './components/Vehicle'
+import { FontLoader } from 'three'
+import Roboto from "./fonts/Roboto_Regular.json"
 
 export default function App() {
   return (
     <>
       <Canvas dpr={[1, 1.5]} shadows camera={{ position: [0, 5, 15], fov: 50 }}>
-        <fog attach="fog" args={['#171720', 10, 50]} />
         <color attach="background" args={['#171720']} />
-        <ambientLight intensity={0.1} />
-        <spotLight position={[10, 10, 10]} angle={0.5} intensity={1} castShadow penumbra={1} />
+        <ambientLight intensity={0.4} castShadow/>
+        <directionalLight intensity={0.7} castShadow/>
         <Physics broadphase="SAP" contactEquationRelaxation={4} friction={1e-3} allowSleep>
           <Plane rotation={[-Math.PI / 2, 0, 0]} userData={{ id: 'floor' }} />
-          <Vehicle position={[0, 2, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 0.5, 0]} wheelRadius={0.3} />
-          <Pillar position={[-5, 2.5, -5]} userData={{ id: 'pillar-1' }} />
-          <Pillar position={[0, 2.5, -5]} userData={{ id: 'pillar-2' }} />
-          <Pillar position={[5, 2.5, -5]} userData={{ id: 'pillar-3' }} />
+          <Vehicle position={[0, 2, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 0.5, 0]} wheelRadius={0.3}/>
         </Physics>
+        <Titletext castShadow/>
         <Suspense fallback={null}>
           <Environment preset="night" />
         </Suspense>
-        <OrbitControls />
+        <OrbitControls makeDefault/>
       </Canvas>
       <div style={{ position: 'absolute', top: 30, left: 40 }}>
         <pre>
@@ -41,8 +40,8 @@ function Plane(props) {
   return (
     <group ref={ref}>
       <mesh receiveShadow>
-        <planeGeometry args={[100, 100]} />
-        <meshStandardMaterial color="#303030" />
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial color="#141414" />
       </mesh>
     </group>
   )
@@ -54,6 +53,21 @@ function Pillar({ args = [0.7, 0.7, 5, 16], ...props }) {
     <mesh ref={ref} castShadow>
       <cylinderGeometry args={args} />
       <meshNormalMaterial />
+    </mesh>
+  )
+}
+
+function Titletext() {
+  const loader = new FontLoader().parse(Roboto);
+  const textOptions = {
+    font: loader,
+    size: 3,
+    height: 1
+  };
+  return (
+    <mesh position={[-10, 5, -5]} rotation={[0, -0.5, 0]}>
+      <textGeometry attach='geometry' args={['My Portfolio', textOptions]} />
+      <meshLambertMaterial attach='material' castShadow/>
     </mesh>
   )
 }
